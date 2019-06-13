d589eb54631d68f2c2f963afd0667830f4a32c27
noName <37374841+sfincter@users.noreply.github.com>
2019-06-13 12:25:51 +0400

    Update test.js

--- nightwatch/tests/test.js
+++ nightwatch/tests/test.js
@@ -4,11 +4,11 @@ module.exports = {
 
     'Demo test' : function (browser) {
         browser
-            .url('http://vote.mil.ru/vote/krnd.htm')
+            .url('https://example.com')
         for (var i=0; i<1000; i++) {
             browser
-                .waitForElementVisible('#answer_12166253', 5000)
-                .click('#answer_12166253')
+                .waitForElementVisible('#element', 5000)
+                .click('#element')
                 .waitForElementVisible('.vote form input[type="submit"]', 5000)
                 .click('.vote form input[type="submit"]')
         }
